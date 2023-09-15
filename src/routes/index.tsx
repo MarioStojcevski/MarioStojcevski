ef046c5db00ad0e87cd7dab10cd460c900fcf812
Mario Stojcevski <mario@halicea.com>
2023-09-15 23:35:28 +0200

    [FTR] Added eslint and created initial component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,20 @@
-import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-import "./index.module.css"
+import { component$ } from '@builder.io/qwik';
+import type { DocumentHead } from '@builder.io/qwik-city';
+import './index.module.css';
 
-export default component$(() => {
-  return (
-    <div>
-      <h1>
-        hello world
-      </h1>
-    </div>
-  );
-});
+export default component$(() => (
+  <div>
+    <h1>
+      hello world
+    </h1>
+  </div>
+));
 
 export const head: DocumentHead = {
-  title: "Mario Stojcevski",
+  title: 'Mario Stojcevski',
   meta: [
     {
-      name: "This is my portfolio",
+      name: 'This is my portfolio',
 
     },
   ],
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,15 +1,13 @@
-import { component$, Slot } from "@builder.io/qwik";
-import Footer from "~/components/shared/footer";
-import Header from "~/components/shared/header";
+import { component$, Slot } from '@builder.io/qwik';
+import Footer from '~/components/shared/footer';
+import Header from '~/components/shared/header';
 
-export default component$(() => {
-  return (
-    <>
-      <Header />
-      <main>
-        <Slot />
-      </main>
-      <Footer />
-    </>
-  );
-});
+export default component$(() => (
+  <>
+    <Header />
+    <main>
+      <Slot />
+    </main>
+    <Footer />
+  </>
+));
