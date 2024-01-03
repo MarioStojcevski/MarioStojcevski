88382edb320019109f5abc60f2167c117304e8b2
Mario Stojcevski <mane.sto4455@gmail.com>
2024-01-03 08:40:50 +0100

    updating package.json

--- adapters/static/vite.config.ts
+++ adapters/static/vite.config.ts
@@ -12,7 +12,7 @@ export default extendConfig(baseConfig, () => {
     },
     plugins: [
       staticAdapter({
-        origin: "https://mariostojcevski.pages.dev",
+        origin: "https://yoursite.qwik.dev",
       }),
     ],
   };
