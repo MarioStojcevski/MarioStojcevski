5dbe3bd036bab747cdb749bb86080cbabb107e5a
Mario Stojcevski <mane.sto4455@gmail.com>
2025-10-28 21:43:43 +0100

    install shadcn

---
+++ src/components/asci-art.tsx
@@ -0,0 +1,29 @@
+type AsciArtProps = {
+  art: string;
+};
+
+export default function AsciArt({ art }: AsciArtProps) {
+  const lines = art.trim().split("\n");
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <pre style={{ whiteSpace: "pre", lineHeight: "1" }}>
+        {lines.map((line, i) => (
+          <div key={line + i}>
+            {line.split("").map((ch, j) => {
+              if (ch === " ") return <span key={line + j}>&nbsp;</span>;
+              return <span key={line + ch + j}>{ch}</span>
+            })}
+          </div>
+        ))}
+      </pre>
+    </div>
+  );
+}
