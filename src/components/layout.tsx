2cc49c169bf60bc0df7f20ff678eeefac647a3b3
Mario Stojcevski <mane.sto4455@gmail.com>
2025-11-03 00:26:44 +0100

    fixing some css

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,10 +2,10 @@ import Navigation from "./navigation";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
+    <div className="mx-5 my-5 md:mx-20 md:my-5 lg:mx-40 lg:my-10 xl:mx-40 xl:my-10">
       <Navigation />
       <main className="py-5">{children}</main>
-    </>
+    </div>
   );
 };
 
