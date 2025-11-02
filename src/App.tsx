7ccc46a9cc859c25fc1ea4876fe6679a0e1064b2
Mario Stojcevski <mane.sto4455@gmail.com>
2025-11-02 15:08:08 +0100

    fix shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import AsciArt from "./components/asci-art";
+import { Button } from "./components/ui/button";
 import fineArts from "./constants/fine-arts";
 
 const App = () => {
   return (
     <div>
+      <Button>test</Button>
       {fineArts.map((x) => <AsciArt key={x} art={x} />)}                  
     </div>
   )
