8973eee40c03364f02d5c4876e1a4725b7810161
Mario Stojcevski <mane.sto4455@gmail.com>
2025-11-02 17:24:02 +0100

    retarded fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,16 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from "react-router";
 
-import Home from './pages/Home';
 import AboutMe from './pages/about-me';
 import Projects from './pages/projects';
 import Art from './pages/art';
-
+import Home from './pages/home';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        <Route path='index' element={<Home />} />
         <Route path='about-me' element={<AboutMe />} />
         <Route path='projects' element={<Projects />} />
         <Route path='art' element={<Art />} />
